9b35b8cada4d0ca5c0884533771052989c0f4a2d
dileepa-css <dileepa@ceylonsoft.jp>
2024-12-12 23:29:49 +0530

    comments

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,11 @@
+// Fill companion data
 document.getElementById('fill-companion-data').addEventListener('click', () => {
     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
         chrome.tabs.sendMessage(tabs[0].id, {action: 'fillCompanionData'});
     });
 });
 
+// Fill credit card data
 document.getElementById('fill-creditcard-data').addEventListener('click', () => {
     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
         chrome.tabs.sendMessage(tabs[0].id, {action: 'fillCreditCardData'});
